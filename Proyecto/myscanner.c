cd003f2352f17f96380fa01f82fe76ccff39fabc
Xbolanos <xbolanosfonseca@gmail.com>
2017-04-24 12:30:55 -0600

    Arreglitos 2

--- Proyecto/myscanner.c
+++ Proyecto/myscanner.c
@@ -163,58 +163,41 @@ void include(FILE* archivoActual,FILE* archivoTemporal, int ntoken){
 			printf("Entra al include. \n");
 	        char *includeArreglado; //El valor del include sin los ""
             ntoken = nextToken(); //Se obtiene el siguiente token para evaluar 
-           	
             /*
                 Debido a que los define son literales
             */  
 
-            if(ntoken == 7){
-            
+            if(ntoken == 7){      
                 
                 numIncludes++;
-
                 FILE* siguienteArchivo; //Se creará un nuevo archivo
-
                 includeArreglado = modificarInclude(yytext); //Se obtiene el include sin ""
-                
-
-
                 /*
                     Si el include es diferente al archivo que lo invocó
                 */
-
-                if(existeInclude(includeArreglado) == false){
-                    
+                if(existeInclude(includeArreglado) == false){                    
                     includes[numIncludes] = includeArreglado; //Se agrega a la tabla
                     siguienteArchivo = fopen(includeArreglado, "r"); //Se pone en modo lectura, FALTA: función que quite los "" de un literal
-                    
-                
+               
                     /*
                         Se le indica a flex cuál es el archivo actual que se está leyendo
                     */
-
-                    yyin = siguienteArchivo; 
-                  
+                    yyin = siguienteArchivo;                   
                     preprocesador1(siguienteArchivo,archivoTemporal); //Se llama de nuevo a la función, pero esta vez con el siguiente archivo incluído
                     fclose(siguienteArchivo);
                     /*
                         Después de la llamada recursiva, se borra la inclusión
                     */
-                    
                     includes[numIncludes] = ""; 
                     numIncludes--;
-
                     yyin =archivoActual; //Se le dice a flex cuál archivo se estará leyendo
 
                 }
-            
-
                 /*
                     Si el include es igual al archivo que lo invocó
                 */
 
                 else{
-
                     printf("Inclusión duplicada de %s", includeArreglado); //Se notifica el error, revisar esta validación
                 }
             }
@@ -244,8 +227,7 @@ char* constantfolding(char* num, char* operator, char* num2){
 
 	}
 	char *rfinal = (char*) malloc(sizeof(result));
-    rfinal[0] = '\0';
-   
+    rfinal[0] = '\0';   
 	sprintf(rfinal,"%d",result);
 	return rfinal;
 
@@ -255,8 +237,7 @@ int define(int ntoken){
 	char* identifiers[50000];
 	ntoken=nextToken();
 	int position=0;
-	char *variable="";
-	
+	char *variable="";	
 	while(ntoken!=ENDLINE){
 		printf("Estoy en el define con:%s\n",yytext);
 		if(strcmp(variable,"")==0 && ntoken==IDENTIFIER){
@@ -274,19 +255,16 @@ int define(int ntoken){
 			if (ntoken==OPERATOR){
 				printf("Valor2:%s\n",yytext);
 				char* operator=(char*)malloc(strlen(yytext));
-				//identifiers=(char*)realloc(identifiers, strlen(operator)+1);
                 strcpy(operator,yytext);
 				
 				ntoken=nextToken();
 				if(ntoken==INTEGER){;
 					printf("Valor3:%s\n",yytext);
 					char* tmpnum=constantfolding(value,operator,yytext);
-					//identifiers=(char*)realloc(identifiers, strlen(tmpnum)+1);
 					strcat(identifiers, tmpnum);
 					strcat(identifiers, " ");
 
 				}else{
-					//identifiers=(char*)realloc(identifiers, strlen(yytext)+1);
 					printf("Valor4:%s\n",yytext);
                     strcat(identifiers, value);
                     strcat(identifiers, " ");
@@ -302,7 +280,6 @@ int define(int ntoken){
 					printf("Problema\n");
 				}
 				printf("Valor5:%s\n",yytext);
-				//identifiers=(char*)realloc(identifiers, strlen(value)+strlen(yytext)+2);
 				strcat(identifiers, value);
 				strcat(identifiers, " ");
 				strcat(identifiers, yytext);
@@ -314,51 +291,29 @@ int define(int ntoken){
 		else if(strcmp(variable,"")==1 &&existeDefine(yytext)!=-1){
 
 			char* tmp[50000];
-		
-			//tmp=(char*)malloc(strlen(defines[existeDefine(yytext)].vDefine));
 			strcpy(tmp,defines[existeDefine(yytext)].vDefine);
-			//identifiers=(char*)realloc(identifiers, strlen(tmp)+1);
 			strcat(identifiers,tmp);
 			strcat(identifiers, " ");
 
-		}
-	
-
-		
+		}		
 		else if(strcmp(variable,"")!=0){
 		  printf("Valor6:%s\n",yytext);
-          char* temp[50000];
-          printf("Aqui no%d\n", strlen(yytext));
-		  //temp=(char*)realloc(identifiers, strlen(yytext)+1);
-
-		  printf("JA LOS ENGAÑE\n");
-          /*if(temp)
-            identifiers = temp;
-            else
-                printf("Mal puntero %p\n", temp);
-         */
 		  strcat(identifiers, yytext);
 		  strcat(identifiers, " ");
 		  position++;
 		}
-
 		if(strcmp(yytext,"\n")!=0){
-			printf("%s\n",yytext );	
+		
 		   ntoken=nextToken();
-		}else{
-			printf("Se supone que sale\n");
 		}
-        
 
     }
     
 	printf("Sale\n");
-
     defines[numDefines].palabra = (char*)malloc(strlen(variable));
     defines[numDefines].vDefine = (char*)malloc(strlen(identifiers));
     strcpy(defines[numDefines].palabra ,variable);
     strcpy(defines[numDefines].vDefine,identifiers);
-
     numDefines++;
 
 	return 0;
@@ -380,32 +335,13 @@ void yyerror(char *texto,char *simbolo, int linea){
 
 };
 
-void imprimirArchivoEntrada(FILE *temporal){
-    int in_char;
-    while ((in_char = getc(temporal)) != EOF){
-        putchar(in_char);
-    } 
-    putchar('\n');
-}
-
-void limpiarDocumento(char *f){
-	fclose(fopen(f, "w"));
-}
-
-
-
 int main(int argc, char *argv[])
-{
-    
-
-    /*
+{    /*
         Se valida que se hayan digitado los archivos de entrada en la terminal
     */
 	
     if(argc == 1){
-
-        printf("No se ingresó ningún archivo de entrada\n");
-     
+        printf("No se ingresó ningún archivo de entrada\n");     
     }
 
     /*
@@ -421,34 +357,21 @@ int main(int argc, char *argv[])
             Se le indica a flex cuál es el archivo actual que se está leyendo
         */
         yyin = archivoEntrada; 
-       	printf("%d\n",yyleng);
-
-        //scanner();
 		if(tmpfile!=NULL){
-		
-            //scanner();
             preproceso=true;
             preprocesador1(archivoEntrada,tmpfile);
             fclose(tmpfile);
             preproceso=false;
             tmpfile = fopen("tmpfile.c", "r"); //Se llama a la función del preprocesador con el archivo de entrada
         	yyin = tmpfile; 
-        	scanner();
-	       	
+        	scanner();  	
 	    }else{
 	    	printf("Archivo esta nulo\n");
 	    }
-
         printf("Termino\n");
-        
-       
-      
         fclose(tmpfile);
         fclose(archivoEntrada);
-		//remove("tmpfile.c");    
-
-
-        
+		remove("tmpfile.c");    
      }
 
     return 0; 
