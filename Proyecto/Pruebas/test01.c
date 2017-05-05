0d69d246496c3cdb79251b8b8686779ec94a18df
Xbolanos <xbolanosfonseca@gmail.com>
2017-05-05 15:08:45 -0600

    Mas estable

--- Proyecto/Pruebas/test01.c
+++ Proyecto/Pruebas/test01.c
@@ -1,5 +1,7 @@
-//maze.walker=0;
+
 	struct Object getABCD(struct Object object){
+		
+		685==544;
 		Framebuffer = (struct Color **)malloc(Vres * hola);
 
 		Framebuffer = (struct Color **)malloc(Vres * sizeof(struct Color*));
